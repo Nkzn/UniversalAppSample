e16beff8299c8246f8d9f42a78ba7249ef8fdbc5
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2017-10-25 14:27:01 +0900

    Add playground to storybook

--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,8 @@ import { linkTo } from '@storybook/addon-links';
 import { Button, Welcome } from '@storybook/react/demo';
 import InstructionsText from '../src/native/components/InstructionsText';
 
+import "./playground";
+
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
